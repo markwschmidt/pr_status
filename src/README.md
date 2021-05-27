7c93961b9f2c3a05dfe6a9244453a5f1e73ec456
Mark Schmidt <mschmidt@ocient.com>
2021-05-27 13:09:25 +0000

    Mostly working

--- src/README.md
+++ src/README.md
@@ -2,3 +2,65 @@
 
 This is a simple utility that can be used by Ocient employees to automatically check the status of their PRs
 
+## Installation
+
+1. Install libssl-dev
+
+```
+sudo apt-get install -y libssl-dev.
+```
+
+2. Install Rust/Cargo
+
+```
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+```
+
+3. Clone the repo
+
+```
+git clone https://github.com/markwschmidt/pr_status
+```
+
+4. Create a GitHub OAuth Access Token
+
+On Github.com, go to User Settings from the dropdown in the top right.
+
+Then, navigate to Developer Settings->Personal Access Tokens
+
+Click "Generate new token"
+
+Give it a name like "PR Status Checker" and give it all "repo" privileges
+
+Save this token for your own use
+
+5. Setup a secrets.txt
+
+In the root directory of pr_status, create a file called secrets.txt with the following format:
+
+```
+{
+   "github_username": "<GITHUB_USERNAME>",
+   "access_token": "<ACCESS_TOKEN_FROM_STEP_4>" ,
+   "repo": "<REPO_OWNER>/<REPO_NAME>"
+}
+```
+
+6. Set up ntfy
+
+https://github.com/dschep/ntfy
+
+I use the Pushbullet integration to get notifications sent to my phone/desktop
+It's also useful to get notified when a build or test run finishes
+
+7. Run `cargo run`
+
+This will build and run the binary. You should see a "prs.json" file created in the root directory with all 
+
+8. Update cron to run every 5 minutes
+
+Run `crontab -e` and add this line
+
+```
+
+``
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use error_chain::error_chain;
-use std::io::Read;
 use serde::{Deserialize, Serialize};
 use std::fs;
 use reqwest::{header, blocking::Client};
+use log::{info, error};
+use std::process::Command;
+
 
 
 #[derive(Deserialize)]
 struct Config {
     github_username: String,
-    access_token: String
+    access_token: String,
+    repo: String
 }
 
 error_chain! {
@@ -45,22 +48,80 @@ struct ApiPullRequestStatus {
     context: String,
     target_url: String, 
 }
+#[derive(Deserialize, Serialize, Debug, Eq, PartialEq)]
+enum GitlabStatus{
+    Pending,
+    Running,
+    Success,
+    Failure,
+    Invalid
+}
 
 #[derive(Deserialize, Serialize, Debug)]
 struct PullRequestStatus{
     number: i32,
-    state: String,
+    state: GitlabStatus,
+    github_url: String,
     pipeline_url: String
 }
 
-fn is_done(state: &str) -> bool {
+fn is_done(state: &GitlabStatus) -> bool {
     match state {
-        "success" | "failure" => true,
+        GitlabStatus::Success | GitlabStatus::Failure => true,
         _ => false
     }
 }
 
+fn fetch_my_prs(config: &Config, client: &Client) -> Result<Vec<ApiPullRequest>> {
+    Ok(client.get(format!("https://api.github.com/repos/{}/pulls", config.repo))
+        .send()?
+        .json::<Vec<ApiPullRequest>>()?
+        .into_iter()
+        .filter(|pull| pull.user.login == config.github_username)
+        .collect())
+}
+
+fn fetch_gitlab_statuses(pulls: Vec<ApiPullRequest>, client: &Client) -> Result<Vec<PullRequestStatus>> {
+    let mut new_prs: Vec<PullRequestStatus> = vec![];
+    for pull in pulls {
+        let statuses: Vec<ApiPullRequestStatus> = client.get(pull.statuses_url).send()?.json()?;
+        for status in statuses {
+            if status.context == "ci/gitlab/gitlab.com" {
+                info!("{:?}", &status);
+                let state = match status.state.as_str() {
+                    "success" => GitlabStatus::Success,
+                    "failure" => GitlabStatus::Failure,
+                    "pending" => GitlabStatus::Pending,
+                    "running" => GitlabStatus::Running,
+                    _ => { error!("Could not parse gitlab status"); GitlabStatus::Invalid}
+                };
+                new_prs.push(PullRequestStatus { number: pull.number, state: state, pipeline_url: status.target_url, github_url: pull.html_url});
+                break; // Take only the newest
+            }
+        }
+    }
+
+    Ok(new_prs)
+
+}
+
+fn notify_pr_finish(status: &PullRequestStatus) -> Result<()> {
+    let message = format!("PR #{} {} PR Link: {}", status.number, {
+        match status.state {
+            GitlabStatus::Success => "succeeded!",
+            GitlabStatus::Failure=> "failed!",
+            _ => "uhhh"
+        }
+    }, status.github_url);
+    Command::new("ntfy")
+        .args(&["send", &message]) 
+        .output()?;
+    Ok(())
+}
+
 fn main() -> Result<()> {
+
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
     let config : Config =  serde_json::from_str(&fs::read_to_string("secrets.txt")?)?;
 
@@ -69,47 +130,36 @@ fn main() -> Result<()> {
     headers.append(header::AUTHORIZATION, header::HeaderValue::from_str(&format!("Bearer {}", config.access_token))?);
     headers.append(header::USER_AGENT, header::HeaderValue::from_str(&"Mark's Cool PR Checker")?);
 
-    // Fetch the PRs
+    // Setup client
     let client = Client::builder()
         .default_headers(headers)
         .build()?;
-    let all_pulls : Vec<ApiPullRequest> = client.get("https://api.github.com/repos/Xeograph/xgsrc/pulls").send()?.json()?;
-    let my_pulls :Vec<ApiPullRequest> =all_pulls.into_iter().filter(|pull| pull.user.login == config.github_username).collect();
+    
+    // Fetch the PRs
+    let pulls = fetch_my_prs(&config, &client)?;
+    info!("Got {} pulls for user", pulls.len());
 
-    println!("Got {} pulls for user", my_pulls.len());
+    // Fetch gitlab status for each PR
+    let new_prs = fetch_gitlab_statuses(pulls, &client)?;
+    info!("PR statuses: {:?}", new_prs);
 
-
-    // Fetch status for each old pull
-    let mut new_prs: Vec<PullRequestStatus> = vec![];
-    for my_pull in my_pulls {
-        let statuses: Vec<ApiPullRequestStatus> = client.get(my_pull.statuses_url).send()?.json()?;
-        for status in statuses {
-            if status.context == "ci/gitlab/gitlab.com" {
-                println!("{:?}", status);
-                new_prs.push(PullRequestStatus { number: my_pull.number, state: status.state, pipeline_url: status.target_url});
-                break; // Take only the newest
-            }
-        }
-    }
-
-    println!("PR statuses: {:?}", new_prs);
-
-    let mut known_prs : Vec<PullRequestStatus> = serde_json::from_str(&fs::read_to_string("prs.json").unwrap_or("[]".to_string()))?;
+    // Load known prs from file
+    let known_prs : Vec<PullRequestStatus> = serde_json::from_str(&fs::read_to_string("prs.json").unwrap_or("[]".to_string()))?;
     let mut out_prs: Vec<PullRequestStatus> = vec![];
 
     // Merge new pulls and old pulls. Notify if anything from old pulls is now done
     for new_pr in new_prs {
         for known_pr in &known_prs {
             if new_pr.number == known_pr.number {
-                // TODO figure out DONE
                 if is_done(&new_pr.state) && !is_done(&known_pr.state) {
-                    // TODO notify
-                    println!("Notifying!");
+                    info!("Notifying!");
+                    notify_pr_finish(&new_pr)?;
                 }
             }
         }
-        out_prs.push(PullRequestStatus { number: new_pr.number, state: new_pr.state, pipeline_url: new_pr.pipeline_url});
+        out_prs.push(new_pr);
     }
     fs::write("prs.json", serde_json::to_string_pretty(&out_prs)?)?;
+    println!("blah");
     Ok(())
 }
